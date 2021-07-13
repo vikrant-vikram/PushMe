e965311d54245306d029dde9ed8b1ed15894d3ee
root <root@localhost.localdomain>
2021-07-13 08:01:33 +0530

    secomnf

--- app.js
+++ app.js
@@ -55,6 +55,12 @@ app.post('/*',function(req,res){
   res.send("no such page");
 });
 
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
+
+
 
 io.on('connection', (socket) => {
 
